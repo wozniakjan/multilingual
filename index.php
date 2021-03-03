cb831f68d0b13dd0c618f374cf1ffe80ce5ffa2f
Jan Wozniak <wozniak.jan@gmail.com>
2021-03-03 13:39:49 +0100

    Test commit for DDEV-Live trigger

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  * The PHP page that serves all page requests on a Drupal installation.
  *
  * All Drupal code is released under the GNU General Public License.
- * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory.
+ * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory. test
  */
 
 use Drupal\Core\DrupalKernel;
